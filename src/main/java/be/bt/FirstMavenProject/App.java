a1b7f5182e771fc1edf718458d722cc2e09d99e9
bizimunda <yourbelover@gmail.com>
2019-12-12 12:15:35 +0100

    first commit

---
+++ src/main/java/be/bt/FirstMavenProject/App.java
@@ -0,0 +1,17 @@
+package be.bt.FirstMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for (int i = 1; i < 6; i++) {
+    		System.out.println( "Hello World "+i );
+			
+		}
+        
+    }
+}
